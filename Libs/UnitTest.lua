5312ff4c818f969df9e7d10be9b9cdc6155793e4
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-03-07 19:20:39 +0200

    Revamp UnitTest
    * Support scopes of tests.
    * Support custom results handlers by passing resultsHandler to Run.
    * Support inspecting the tests cases before execution through IterableTestsCases.

--- Libs/UnitTest.lua
+++ Libs/UnitTest.lua
@@ -6,7 +6,44 @@ assert(C, "UnitTest-1.0 requires Contracts-1.0")
 local lib = LibStub:NewLibrary("UnitTest-1.0", 0)
 if not lib then return end
 
-local Tests = {}
+local TestsCases = {}
+
+local function AddTestCase(scope, testName, testFunc)
+    C:IsTable(scope, 2)
+    C:IsString(testName, 3)
+    C:IsFunction(testFunc, 4)
+    local testCase = {
+        parent = scope,
+        name = testName,
+        func = testFunc
+    }
+    table.insert(TestsCases, testCase)
+end
+
+local function ExecuteTestCase(self, name, func, resultsHandler, ...)
+	local success, err = pcall(func, self, ...)
+    if not success then
+        resultsHandler("TestCase", name, err)
+    end
+    return success
+end
+
+local function PrintHandler(type, ...)
+    if type == "Scope" then
+        local name = ...
+        print("------------------------------------------------------------")
+        print("+ " .. name)
+    elseif type == "TestCase" then
+        local name, err = ...
+        print("   ---")
+        print("   + " .. name .. ": " .. err)
+    elseif type == "Summary" then
+        print("------------------------------------------------------------")
+        local totalTests, passedCounter, failedCounter = ...
+        print(("Total Tests: %d Passed: %d Failed: %d"):format(totalTests, passedCounter, failedCounter))
+        print("------------------------------------------------------------")
+    end
+end
 
 function lib:Assert(condition)
     assert(condition, "Assertion failed: The test condition should have been true, but it was false.")
@@ -21,32 +58,46 @@ function lib:Capture(func, showError)
     assert(not success, "Capture failed: The function should have thrown an error, but it did not.")
 end
 
-function lib:Test(name, func)
-    C:IsString(name, 2)
-    C:IsFunction(func, 3)
-    local testCase = {
-        name = name,
-        func = func
+function lib:CreateScope(scopeName)
+    C:IsString(scopeName, 2)
+    local scope = {
+        __name = scopeName
     }
-    table.insert(Tests, testCase)
+    return setmetatable(scope, {  __newindex = function(self, testName, testFunc)
+        AddTestCase(self, testName, testFunc)
+    end })
 end
 
-local function ExecuteTest(self, name, func, ...)
-	local success, err = pcall(func, self, ...)
-    if not success then
-        print(name .. ":\n" .. err)
-    end
-    return success
-end
-
-function lib:Run()
-    local totalTests, passedCounter = #Tests, 0
-    for _, testCase in ipairs(Tests) do
-        if not ExecuteTest(self, testCase.name, testCase.func) then
-            return
-        else
-            passedCounter = passedCounter + 1
+function lib:IterableTestsCases()
+    local i = 1
+    local n = #TestsCases
+    local scope = nil
+    return function()
+        while i <= n do
+            local testCase = TestsCases[i]
+            if not scope or testCase.parent ~= scope then
+                scope = testCase.parent
+                return "Scope", testCase.parent.__name
+            end
+            i = i + 1
+            return "TestCase", testCase.name, testCase.func
         end
     end
-    print(("Total Tests: %d Passed: %d."):format(totalTests, passedCounter))
+end
+
+function lib:Run(resultsHandler)
+    resultsHandler = resultsHandler or PrintHandler
+    local totalTests, passedCounter, failCounter = #TestsCases, 0, 0
+    for type, name, func in self:IterableTestsCases() do
+        if type == "Scope" then
+            resultsHandler(type, name)
+        elseif type == "TestCase" then
+            if ExecuteTestCase(self, name, func, resultsHandler) then
+                passedCounter = passedCounter + 1
+            else
+                failCounter = failCounter + 1
+            end
+        end
+    end
+    resultsHandler("Summary", totalTests, passedCounter, failCounter)
 end
--- Tests/Addon/AddonTests.lua
+++ Tests/Addon/AddonTests.lua
@@ -1,29 +1,33 @@
 local U = LibStub("UnitTest-1.0")
+
 local addon = LibStub("Addon-1.0")
 
-do -- New
-    U:Test("New: Should return the passed table", function(self)
+do
+    local New = U:CreateScope("New")
+
+    New["Should return the passed table"] = function(self)
         local table = {}
         local actualTable = addon:New("WowLibs", table)
 
-        self:Assert(actualTable == table)
-    end)
+        self:Assert(actualTable ~= table)
+    end
 
-    U:Test("New: Should throw when addonName is not a string", function(self)
+    New["Should throw when addonName is not a string"] = function(self)
         self:Capture(function()
-            addon:New(nil, {})
+            addon:New("WowLibs", {})
         end)
-    end)
+    end
 
-    U:Test("New: Should throw when addonName is not valid", function(self)
+    New["Should throw when addonName is not valid"] = function(self)
         self:Capture(function()
             addon:New("Foo", {})
         end)
-    end)
+    end
 
-    U:Test("New: Should throw when addonTable is not a table", function(self)
+    New["Should throw when addonTable is not a table"] = function(self)
         self:Capture(function()
             addon:New("WowLibs", nil)
         end)
-    end)
+    end
 end
+
