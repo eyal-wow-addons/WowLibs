0613ec538d11eac5984cf160e8e8e6c238b15a9a
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-03-15 08:39:42 +0200

    Replace LifeCycleTest with a UnitTest test
    * Move all tests directly to the Tests folder as opposed to having a subfolder.

---
+++ Tests/AddonTests.lua
@@ -0,0 +1,105 @@
+local T = LibStub("UnitTest-1.0"):Test("Addon-1.0")
+
+do
+    local Addon = T:Test("Addon")
+
+    local eventsOrder = {
+        ["addon:OnInitializing"] = 1,
+        ["object:OnInitializing"] = 2,
+        ["addon:OnInitialized"] = 3,
+        ["object:OnInitialized"] = 4,
+        ["object:RegisterCallback"] = 5,
+        ["object:RegisterEvents:PLAYER_LOGIN"] = 6,
+        ["object:RegisterEvent:CUSTOM_EVENT:PLAYER_LOGIN"] = 7,
+        ["object:RegisterEvents:PLAYER_ENTERING_WORLD"] = 8,
+        ["object:RegisterEvent:CUSTOM_EVENT:PLAYER_ENTERING_WORLD"] = 9
+    }
+
+    local actualEventsOrder = {}
+
+    local addon = T:Ref():New("WowLibs", {})
+    local LifeCycleObjectTest = addon:NewObject("LifeCycleObjectTest")
+
+    function addon:OnInitializing()
+        table.insert(actualEventsOrder, "addon:OnInitializing")
+    end
+    
+    function addon:OnInitialized()
+        table.insert(actualEventsOrder, "addon:OnInitialized")
+    end
+    
+    function LifeCycleObjectTest:OnInitializing()
+        table.insert(actualEventsOrder, "object:OnInitializing")
+    end
+    
+    function LifeCycleObjectTest:OnInitialized()
+        table.insert(actualEventsOrder, "object:OnInitialized")
+    end
+    
+    LifeCycleObjectTest:RegisterCallback(function()
+        table.insert(actualEventsOrder, "object:RegisterCallback")
+    end)
+    
+    LifeCycleObjectTest:RegisterEvents(
+        "PLAYER_LOGIN",
+        "PLAYER_ENTERING_WORLD", function(self, eventName)
+            table.insert(actualEventsOrder, ("object:RegisterEvents:%s"):format(eventName))
+            self:TriggerEvent("CUSTOM_EVENT", eventName)
+        end)
+    
+    LifeCycleObjectTest:RegisterEvent("CUSTOM_EVENT", function(self, eventName, arg1)
+        table.insert(actualEventsOrder, ("object:RegisterEvent:%s:%s"):format(eventName, arg1))
+    end)
+
+    Addon["Should fire the events in the correct order"] = function(self)
+        local isEventsOrdered = false
+
+        for index, value in ipairs(actualEventsOrder) do
+            isEventsOrdered = false
+            if eventsOrder[value] == index then
+                isEventsOrdered = true
+            else
+                break
+            end
+        end
+
+        T:Assert(isEventsOrdered == true)
+    end
+end
+
+do
+    local New = T:Test("New")
+
+    New["Should return the passed table"] = function(self)
+        local tbl = {}
+
+        self:Delete("Addon1")
+
+        local actualTable = self:New("Addon1", tbl)
+
+        T:Assert(actualTable == tbl)
+    end
+
+    New["Should return nil when the addon was already created"] = function(self)
+        local tbl = {}
+        
+        self:New("Addon2", tbl)
+        
+        local actualTable = self:New("Addon2", tbl)
+
+        T:Assert(actualTable == nil)
+    end
+
+    New["Should throw when addonName is not a string"] = function(self)
+        T:Capture(function()
+            self:New(nil, {})
+        end)
+    end
+
+    New["Should throw when addonTable is not a table"] = function(self)
+        T:Capture(function()
+            self:New("WowLibs", nil)
+        end)
+    end
+end
+
