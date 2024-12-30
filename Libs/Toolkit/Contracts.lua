55c82d593970c952150ad9c9226100f0b3f2566b
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-12-31 00:06:54 +0200

    Fix Contracts
    Fix a case where one of the arguments is nil format fails and throw an error.

--- Libs/Toolkit/Contracts.lua
+++ Libs/Toolkit/Contracts.lua
@@ -7,6 +7,7 @@ if not lib then return end
 
 local error, assert = error, assert
 local select = select
+local unpack = unpack
 local sjoin = string.join
 local smatch = string.match
 local type = type
@@ -21,7 +22,13 @@ function lib:Ensures(condition, message, ...)
     end
 
     if not condition then
-        assert(condition, message:format(...))
+        local args = {...}
+        for i = 1, select("#", ...) do
+            if args[i] == nil then
+                args[i] = "???"
+            end
+        end
+        assert(condition, message:format(unpack(args)))
     end
 end
 
